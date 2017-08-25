4bfa9faef9ca3c34a34d5b37322399187994bb77
Jimmy Karl Roland Wärting <jimmy@warting.se>
2017-08-25 14:37:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ module.exports = function resolve(target) {
     get(noop, key) {
       return key === 'then' ? target.then.bind(target) : resolve(
         Promise.resolve(target).then(target => {
-          if (typeof target[key] === 'function') return target[key].bind(target)
+          if (typeof target[key] === 'function') 
+            return target[key].bind(target)
           return target[key]
         })
       )

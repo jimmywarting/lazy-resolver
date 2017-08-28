c288c58cb939344010a5f1d56b7eb9c42bb4a285
Jimmy Karl Roland Wärting <jimmy@warting.se>
2017-08-28 15:55:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ module.exports = function resolve(target) {
   return new Proxy(() => {}, {
     get(noop, key) {
       return key === 'then' ? target.then.bind(target) : resolve(
-        Promise.resolve(target).then(target => {
-          if (typeof target[key] === 'function') 
-            return target[key].bind(target)
-          return target[key]
-        })
+        Promise.resolve(target).then(target => 
+          typeof target[key] === 'function' 
+            ? target[key].bind(target)
+            : target[key]
+        )
       )
     },
 
     apply(noop, that, args) {
-      return resolve(target.then(result => {
-        return result.apply(that, args)
-      }))
+      return resolve(target.then(result => 
+        result.apply(that, args)
+      ))
     }
   })
 }
